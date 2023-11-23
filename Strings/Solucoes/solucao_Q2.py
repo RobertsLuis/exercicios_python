b033d292f1559e20a8d55969f01a170c07df7046
RobertsLuis <bejr2002@gmail.com>
2023-11-23 08:28:25 -0300

    Resolução e testes Q2

---
+++ Strings/Solucoes/solucao_Q2.py
@@ -0,0 +1,37 @@
+""" 
+Questão:
+https://www.hackerrank.com/challenges/text-wrap/problem?isFullScreen=true
+You are given a string, line width and paragraph height.
+Your task is to wrap the string into paragraph(s).
+
+**You can't return a half word
+"""
+
+
+def organizar_texto_em_paragrafo(texto, largura_linha, altura_paragrafo):
+    """Quebra os textos em linhas e paragrafos
+
+    Args:
+        texto str: Texto que sofrerá a organização
+        largura_linha str: Limite de letras em cada linha
+        altura_paragrafo str: Limite de linhas em cada paragro
+    Returns:
+        str: Texto organizado em linhas e paragrafos
+
+    """
+    palavras = texto.split()
+    info_palavras = [(palavra, len(palavra)) for palavra in palavras]
+    print(palavras)
+    linhas = [""]
+    for palavra, tamanho_palavra in info_palavras:
+        tamanho_linha_atual = len(linhas[-1])
+        if tamanho_linha_atual + tamanho_palavra <= largura_linha:
+            linhas[-1] += f" {palavra}"
+        else:
+            linhas.append(
+                f"{palavra}"
+                if len(linhas) % altura_paragrafo > 0
+                else f"\n{palavra.capitalize()}"
+            )
+
+    return "\n".join(linhas).replace(" ", "", 1)
---
+++ Strings/Testes/teste_Q2.py
@@ -0,0 +1,25 @@
+import unittest
+import os
+import sys
+
+# Adiciona o diretório Solucoes ao sys.path
+diretorio_atual = os.path.dirname(os.path.abspath(__file__))
+diretorio_solucoes = os.path.join(diretorio_atual, "..", "Solucoes")
+sys.path.append(diretorio_solucoes)
+
+from solucao_Q2 import organizar_texto_em_paragrafo
+
+
+class TestExpandirRange(unittest.TestCase):
+    def test_organizar_texto(self):
+        textos_teste = [
+            (("Cinco Cinco Cinco Cinco Cinco Cinco Cinco Cinco", 12, 2), "Cinco Cinco\nCinco Cinco\n\nCinco Cinco\nCinco Cinco"),
+        ]
+        for argumentos_modelo, resultado_correto in textos_teste:
+            with self.subTest(textos=argumentos_modelo):
+                texto, linha, paragrafo = argumentos_modelo
+                resultado = organizar_texto_em_paragrafo(texto, linha, paragrafo)
+                self.assertEqual(resultado, resultado_correto)
+
+if __name__ == "__main__":
+    unittest.main()
