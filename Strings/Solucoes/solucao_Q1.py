fdfc649e3b2383e832e461031e7c21b241539767
RobertsLuis <bejr2002@gmail.com>
2023-11-23 08:12:38 -0300

    Inserção de intervalos de números na resolução da questão como extra.

--- Strings/Solucoes/solucao_Q1.py
+++ Strings/Solucoes/solucao_Q1.py
@@ -6,25 +6,39 @@ Note that if the range is given in capital letters, return the string in capital
 """
 
 import re
-def expandir_range(intervalo):
-    """Gera todas as letras contidas no intervalo especificado 'x-y'
+def expandir_intervalo(intervalo):
+    """Gera todos os elementos contidos no intervalo especificado 'x-y', em ordem crescente de valor
     
-    **Sensível a letras maísculas e minúsculas
+    **Sensível a letras maísculas e minúsculas 
     
     Args:
-        range str: 'a-z'
+        range str: Uma string que deve seguir o padrão 'letra-letra' ou 'numero-numero'
 
     Returns:
-        str: todas as letras dentro do range especificado
+        str: Todos os caracteres contidos no intervalo especificado
         
     """
-    padrao = re.compile(r'^[a-zA-Z]-[a-zA-Z]$')
-    if not(padrao.match(intervalo)):
-        raise Exception('O intervalo especificado está fora do padrão "a-z"/"A-z"/"A-Z"')
+    padraoLetras = re.compile(r'^[a-zA-Z]-[a-zA-Z]$')
+    padraoNumeros = re.compile(r'^\d+-\d+$')
+
+    if padraoLetras.match(intervalo):
+        return __expandir_intervalo_letras(intervalo)
+    elif padraoNumeros.match(intervalo):
+        return __expandir_intervalo_numeros(intervalo)
+    
+    else:
+        raise Exception('O intervalo especificado está fora dos padrões aceitados \n"a-z"/"A-z"/"A-Z" ou "0-100" "20-0"')
+
+def __expandir_intervalo_letras(intervalo):
     inicio, fim = intervalo.split('-')
     if ord(inicio) > ord(fim):
         fim, inicio = inicio, fim
     expansao = ''.join(chr(i) for i in range(ord(inicio), ord(fim)+1))
     return re.sub(r'[^a-zA-Z]', '',expansao)
 
-print(expandir_range('z-Z'))
+def __expandir_intervalo_numeros(intervalo):
+    inicio, fim = intervalo.split('-')
+    if int(inicio) > int(fim):
+        fim, inicio = inicio, fim
+    expansao = ', '.join(str(i) for i in range (int(inicio), int(fim)+1))
+    return expansao
--- Strings/Testes/teste_Q1.py
+++ Strings/Testes/teste_Q1.py
@@ -7,29 +7,31 @@ diretorio_atual = os.path.dirname(os.path.abspath(__file__))
 diretorio_solucoes = os.path.join(diretorio_atual, "..", "Solucoes")
 sys.path.append(diretorio_solucoes)
 
-from solucao_Q1 import expandir_range
+from solucao_Q1 import expandir_intervalo
 
 
 class TestExpandirRange(unittest.TestCase):
-    def test_expandir_range_valido(self):
+    def test_expandir_intervalo_valido(self):
         intervalos_teste = [
             ("A-C", "ABC"),
             ("Z-c", "Zabc"),
             ("c-Z", "Zabc"),
             ("A-a", "ABCDEFGHIJKLMNOPQRSTUVWXYZa"),
+            ("4-11", "4, 5, 6, 7, 8, 9, 10, 11"),
+            ("15-8", "8, 9, 10, 11, 12, 13, 14, 15")
         ]
         for intervalo_modelo, resultado_correto in intervalos_teste:
             with self.subTest(intervalo=intervalo_modelo):
-                resultado = expandir_range(intervalo_modelo)
+                resultado = expandir_intervalo(intervalo_modelo)
                 self.assertEqual(resultado, resultado_correto)
 
     def test_expandir_range_formato_invalido(self):
         with self.assertRaises(Exception) as contexto:
-            expandir_range("ac")
+            expandir_intervalo("ac")
 
         self.assertEqual(
             str(contexto.exception),
-            'O intervalo especificado está fora do padrão "a-z"/"A-z"/"A-Z"',
+            'O intervalo especificado está fora dos padrões aceitados \n"a-z"/"A-z"/"A-Z" ou "0-100" "20-0"',
         )
 
 
